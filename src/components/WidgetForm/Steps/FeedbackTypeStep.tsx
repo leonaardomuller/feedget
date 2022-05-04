cf8cfda119de29cf4e26b9f6841ba4a995f3c38f
leonaardomuller <leonaardo.muller@gmail.com>
2022-05-04 20:13:39 -0300

    Stage 2 finalized #NextLevelWeek

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -1,11 +1,13 @@
-import { FeedbackType, feedbackTypes } from ".."
-import { CloseButton } from "../../CloseButton"
+import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
 
 interface FeedbackTypeStepProps {
-  onFeedbackTypeChanged: (type: FeedbackType) => void; 
+  onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
 
-export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProps) {
+export function FeedbackTypeStep({
+  onFeedbackTypeChanged,
+}: FeedbackTypeStepProps) {
   return (
     <>
       <header>
@@ -14,20 +16,20 @@ export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProp
       </header>
 
       <div className="flex py-8 gap-2 w-full">
-          {Object.entries(feedbackTypes).map(([key, value]) => {
-            return (
-              <button
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
               key={key}
-                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
-                type="button"
-              >
-                <img src={value.image.source} alt={value.image.alt}/>
-                <span>{value.title}</span>
-              </button>
-            )
-          })}      
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
       </div>
     </>
-  )
+  );
 }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,58 +1,74 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { FeedbackTypeStep } from './Steps/FeedbackTypeStep'
- 
-import bugImageUrl from '../../assets/bug.svg'
-import bulbImageUrl from '../../assets/bulb.svg'
-import thoughtImageUrl from '../../assets/thought.svg'
-import { FeedbackContentStep } from './Steps/FeedbackContentStep'
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+
+import bugImageUrl from "../../assets/bug.svg";
+import bulbImageUrl from "../../assets/bulb.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
   BUG: {
     title: "Problema",
     image: {
       source: bugImageUrl,
-      alt: 'Imagem de um inseto'
-    }
+      alt: "Imagem de um inseto",
+    },
   },
   IDEA: {
     title: "Idéia",
     image: {
       source: bulbImageUrl,
-      alt: 'Imagem de uma lâmpada'
-    }
+      alt: "Imagem de uma lâmpada",
+    },
   },
   OTHER: {
     title: "Outro",
     image: {
       source: thoughtImageUrl,
-      alt: 'Imagem de uma nuvem de pensamento'
-    }
+      alt: "Imagem de uma nuvem de pensamento",
+    },
   },
-}
+};
 
-export type FeedbackType = keyof typeof feedbackTypes
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
-  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   function handleRestartFeedback() {
-    setFeedbackType(null)
+    setFeedbackType(null);
+    setFeedbackSent(false);
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-      ) : (
-        <FeedbackContentStep 
-        feedbackType={feedbackType}
-        onFeedbackRestartRequested={handleRestartFeedback}
+      {feedbackSent ? (
+        <FeedbackSuccessStep
+          onFeedbackRestartRequested={handleRestartFeedback}
         />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
       )}
       <footer className="text-xs text-neutral-400">
-        Feito com 💜 {" "} 
-        <a className="underline underline-offset-2" href="https://rocketseat.com.br" target="_blank">
+        Feito com ♡ pela{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://rocketseat.com.br"
+          target="_blank"
+        >
           Rocketseat
         </a>
       </footer>
